1e20d11852809a7fd3742004e86233cffce302a4
Richard Chien <rchien@gmail.com>
2022-02-09 07:02:35 -0800

    add other shopify paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     content: [
         "./src/**/*.{liquid,json}",
+        "./sections/**/*.{liquid,json}",
+        "./snippets/**/*.{liquid,json}",
+        "./layout/**/*.{liquid,json}",
     ],
     theme: {
         extend: {},
